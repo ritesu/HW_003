00c91a98fed46814d88987e2edb7f30b47051621
ritesu <ritesu@mail.ru>
2023-01-27 05:52:19 +1100

    Task_003

---
+++ Task_003/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число для возвдения его в степень");
+int a = int.Parse(Console.ReadLine()?? "0");
+Console.WriteLine("Введите степень");
+int b = int.Parse(Console.ReadLine()?? "0");
+int[] numbers = new int[b];
+int c = a;
+int d = 0;
+{
+    Console.Write(b + " ");
+}
+Console.WriteLine();
+Console.Write(a + " ");
+for(int i = 1; i < numbers.Length; i++)
+{
+    d = c*a;
+    c = d;
+    Console.Write(c + " ");
+}
+Console.WriteLine();
+Console.WriteLine("Результат равен {0}", c);
