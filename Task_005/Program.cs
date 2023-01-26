50cb62a7dfcb912f9b0e31776f07169b95d91e90
ritesu <ritesu@mail.ru>
2023-01-27 06:44:52 +1100

    Task_005

---
+++ Task_005/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите до какого числа выполнить произведение");
+int[] numbers = Enumerable.Range(1, 99).ToArray();
+int a = int.Parse(Console.ReadLine()?? "");
+Array.Resize(ref numbers, a);
+int b = 1;
+foreach (int i in numbers)
+{
+    Console.Write(i + " ");
+}
+Console.WriteLine();
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    b = b*numbers[i];
+    Console.Write(b + " ");
+}
+Console.WriteLine();
+Console.Write("Произведение чисел равна {0}", b);
