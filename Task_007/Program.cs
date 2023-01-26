b09d6c131beb3de38b9da5dc9e58699db312ed34
ritesu <ritesu@mail.ru>
2023-01-27 07:04:12 +1100

    Task_007

---
+++ Task_007/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] numbers = new int [8];
+for (int i = 0; i < numbers.Length; i++)
+{
+    int num = new Random().Next(0,2);
+    numbers[i] = num;
+}
+foreach (int i in numbers)
+{
+    Console.Write(i + " ");
+}
