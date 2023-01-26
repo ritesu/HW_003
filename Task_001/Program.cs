7ab26743671a15ebfeb43152a9af8f2cf6a54b30
ritesu <ritesu@mail.ru>
2023-01-27 04:33:19 +1100

    Task_001

---
+++ Task_001/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите размер массива");
+int a = int.Parse(Console.ReadLine()?? "");
+int[] numbers = new int[a];
+for (int i = 0; i < numbers.Length; i++)
+{
+    int b = new Random().Next(0, 9);
+    numbers[i] = b;
+}
+foreach (int i in numbers)
+{
+    Console.Write(i + " ");
+}
+Console.WriteLine();
+for (int i = 0; i < numbers.Length; i++)
+{
+    int c = numbers[i]*numbers[i]*numbers[i];
+    Console.Write(c + " ");
+}
