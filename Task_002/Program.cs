af1d1abdff126d7facbb07baa16607d1560a7925
ritesu <ritesu@mail.ru>
2023-01-27 04:39:14 +1100

    Task_002

---
+++ Task_002/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите размер массива");
+int a = int.Parse(Console.ReadLine()?? "");
+int[] numbers = new int[a];
+int c = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    int b = new Random().Next(0, 9);
+    numbers[i] = b;
+}
+foreach (int i in numbers)
+{
+    Console.Write(i + " ");
+}
+Console.WriteLine();
+for (int i = 0; i < numbers.Length; i++)
+{
+    c = numbers[i] + c;
+}
+Console.WriteLine("Сумма всех чисел равна {0}",c);
