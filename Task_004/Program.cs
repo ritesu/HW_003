27c3fa9144ef5f33399e0fa14c7333882d66af2e
ritesu <ritesu@mail.ru>
2023-01-27 05:59:11 +1100

    Task_004

---
+++ Task_004/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine()?? "0");
+int c = 0;
+while (a > 0)
+{
+    int b = a % 10;
+    c = c + b;
+    a = a / 10;
+}
+Console.WriteLine("Сумма цифр в числе равна {0}", c);
